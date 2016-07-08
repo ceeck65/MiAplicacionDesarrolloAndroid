5b347ffccb3d1727cda3b30c624ddbac155bd656
Mario Fernandez <alespcs@gmail.com>
2016-07-08 17:40:57 -0400

    Finalizado el proyecto

--- app/src/main/java/ve/com/disproweb/android/myapplication/DetailsActivity.java
+++ app/src/main/java/ve/com/disproweb/android/myapplication/DetailsActivity.java
@@ -1,20 +1,94 @@
 package ve.com.disproweb.android.myapplication;
 
-import android.support.design.widget.TextInputEditText;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class DetailsActivity extends AppCompatActivity {
 
-    Button btnNext;
-    TextInputEditText txtName, txtPhone, txtEmail, txtDescription;
-    DatePicker datePicker;
+public class DetailsActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnDetails;
+    TextView tvName, tvBirthDay, tvPhone, tvEmail, tvDescription;
+    DatePicker birthDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        tvName  = (TextView) findViewById(R.id.tvName);
+        tvBirthDay = (TextView) findViewById(R.id.tvBirthDay);
+        tvPhone = (TextView) findViewById(R.id.tvPhone);
+        tvEmail = (TextView) findViewById(R.id.tvEmail);
+        tvDescription = (TextView) findViewById(R.id.tvDescription);
+        btnDetails = (Button) findViewById(R.id.btnEditDetails);
+        btnDetails.setOnClickListener(this);
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+
+
+        if (bundle != null) {
+            // Asignación de Variables
+            String name  = (String) bundle.get("Name");
+            String Phone = (String) bundle.get("Phone");
+            String Email = (String) bundle.get("Email");
+            String Description = (String) bundle.get("Description");
+
+            Integer day      =  bundle.getInt("day");
+            Integer month    =  bundle.getInt("month");
+            Integer year     =  bundle.getInt("year");
+
+            // Setear Variables
+            tvName.setText(name);
+            tvBirthDay.setText(String.valueOf(day + "/" + month + "/" + year));
+            tvPhone.setText(Phone);
+            tvEmail.setText(Email);
+            tvDescription.setText(Description);
+        } else {
+            Toast.makeText(getApplicationContext(), "Todos los campos son requeridos", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == findViewById(R.id.btnEditDetails).getId()){
+            Intent intentDetails = new Intent(getApplicationContext(), MainActivity.class);
+
+            Intent intent = getIntent();
+            Bundle bundle = intent.getExtras();
+
+            Integer day     =  bundle.getInt("day");
+            Integer month   =  bundle.getInt("month");
+            Integer year    =  bundle.getInt("year");
+
+
+            tvName  = (TextView) findViewById(R.id.tvName);
+            birthDay = (DatePicker) findViewById(R.id.datePicker);
+            tvPhone = (TextView) findViewById(R.id.tvPhone);
+            tvEmail = (TextView) findViewById(R.id.tvEmail);
+            tvDescription = (TextView) findViewById(R.id.tvDescription);
+
+            String  Name        = tvName.getText().toString();
+            String  Phone       = tvPhone.getText().toString();
+            String  Email       = tvEmail.getText().toString();
+            String  Description = tvDescription.getText().toString();
+
+            intentDetails.putExtra("txtName", Name);
+            intentDetails.putExtra("day", day);
+            intentDetails.putExtra("month", month);
+            intentDetails.putExtra("year", year);
+            intentDetails.putExtra("txtPhone", Phone);
+            intentDetails.putExtra("txtEmail", Email);
+            intentDetails.putExtra("txtDescription", Description);
+
+
+            startActivity(intentDetails);
+        }
     }
 }
--- app/src/main/java/ve/com/disproweb/android/myapplication/MainActivity.java
+++ app/src/main/java/ve/com/disproweb/android/myapplication/MainActivity.java
@@ -1,16 +1,20 @@
 package ve.com.disproweb.android.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.TextView;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    DatePicker datePicker;
-    TextView textView;
-    Button btnNext;
+    DatePicker  datePicker;
+    EditText    txtName, txtPhone, txtEmail, txtDescription;
+    Button      btnNext;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,56 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         datePicker = (DatePicker) findViewById(R.id.datePicker);
+        txtName   = (EditText) findViewById(R.id.txtName);
+        txtPhone  = (EditText) findViewById(R.id.txtPhone);
+        txtEmail  = (EditText) findViewById(R.id.txtEmail);
+        txtDescription = (EditText) findViewById(R.id.txtDescription);
+        btnNext = (Button) findViewById(R.id.btnNext);
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+
+        if (bundle != null) {
+            String txt_Name  = (String) bundle.get("txtName");
+            String txt_Phone = (String) bundle.get("txtPhone");
+            String txt_Email = (String) bundle.get("txtEmail");
+            String txt_Description = (String) bundle.get("txtDescription");
+            Integer day     =  bundle.getInt("day");
+            Integer month   =  bundle.getInt("month");
+            Integer year    =  bundle.getInt("year");
+            txtName.setText(txt_Name);
+            txtPhone.setText(txt_Phone);
+            txtEmail.setText(txt_Email);
+            txtDescription.setText(txt_Description);
+            datePicker.updateDate(year, (month - 1), day);
+        }
+
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), DetailsActivity.class);
+                // Asignacion de InputText
+                String  Name        = txtName.getText().toString();
+                String  Phone       = txtPhone.getText().toString();
+                String  Email       = txtEmail.getText().toString();
+                String  Description = txtDescription.getText().toString();
+                Integer day         = datePicker.getDayOfMonth();
+                Integer month       = datePicker.getMonth();
+                Integer year        = datePicker.getYear();
+                Integer monthNew    = (month + 1);
+                // Asignar PutExtras
+                intent.putExtra("Name", Name);
+                intent.putExtra("Phone", Phone);
+                intent.putExtra("Email", Email);
+                intent.putExtra("Description", Description);
+                intent.putExtra("day", day);
+                intent.putExtra("month", monthNew);
+                intent.putExtra("year", year);
+                intent.putExtra("year", year);
+                startActivity(intent);
+            }
+        });
 
 
 
